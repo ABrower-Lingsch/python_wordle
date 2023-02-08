783183347a2f68add0b405587afbcfd24b817e36
Adam Brower-Lingsch <abrowerlingsch@gmail.com>
2023-02-08 11:26:23 -0500

    fixes play again feature

--- wordle.py
+++ wordle.py
@@ -3,6 +3,7 @@ import sys
 from colorama import Fore, Back, init
 init(autoreset=True)
 
+
 def print_menu():
     print(Fore.CYAN + ''' 
  __         ______     ______   ______        ______   __         ______     __  __        __     __     ______     ______     _____     __         ______       
@@ -14,15 +15,17 @@ def print_menu():
     ''')
     print("Enter a five letter word. You get 6 guesses.\n")
 
+
 def read_random_word():
     with open("words.txt") as f:
         words = f.read().splitlines()
         return random.choice(words)
 
-word = read_random_word()
+
 play_again = ""
 
 while play_again != "n":
+    word = read_random_word()
     print_menu()
     for attempt in range(1, 7):
         guess = input().lower()
@@ -30,7 +33,7 @@ while play_again != "n":
         sys.stdout.write('\x1b[1A')
         sys.stdout.write('\x1b[2K')
 
-        for i in range( min(len(guess), 5)):
+        for i in range(min(len(guess), 5)):
             if guess[i] == word[i]:
                 print(Back.GREEN + guess[i], end="")
             elif guess[i] in word:
@@ -40,13 +43,16 @@ while play_again != "n":
         print()
 
         if guess == word:
-            print(Fore.CYAN + "You guessed the word in", attempt, Fore.CYAN +"tries!")
+            print(Fore.CYAN + "You guessed the word in",
+                  attempt, Fore.CYAN + "tries!")
             break
-            
+
         elif attempt == 6:
-            print(Fore.RED + "You ran out of guesses! The word was", word, Fore.RED + ". Better luck next time...")
-            
-    play_again = input(Fore.YELLOW + "Enter 'n' to exit or enter any other key to play again. ")
+            print(Fore.RED + "You ran out of guesses! The word was",
+                  word, Fore.RED + ". Better luck next time...")
+
+    play_again = input(
+        Fore.YELLOW + "Enter 'n' to exit or enter any other key to play again. ")
     if play_again == "n":
         print(Fore.MAGENTA + "Thanks for playing! :) <3")
         break
